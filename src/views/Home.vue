54ae440a10aa937d74813f63950370d5fa0e458b
PC_Aaron <1021048274@qq.com>
2020-08-11 22:35:56 +0800

    feat:封装Topnav组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo">LOGO</div>
-            <ul class="menu">
-                <li>菜单1</li>
-                <li>菜单2</li>
-            </ul>
-        </div>
+        <Topnav/>
         <div class="banner">
             <h1>novel-ui</h1>
             <h2>A Vue.js 3.0 UI Toolkit for Web</H2>
@@ -17,22 +11,16 @@
         </div>
     </div>
 </template>
-<style scoped lang="scss">
-.topnav{
-    background: pink;
-    display: flex;
-    padding: 16px;
-    > .logo{
-        max-width: 6em;
-        margin-right: auto;
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+
+export default {
+    components: {
+        Topnav
     }
-    > .menu{
-        display: flex;
-        white-space: nowrap;
-        flex-wrap: nowrap;
-        > li{
-            margin: 0 1em;
-        }
-    }
+    
 }
+</script>
+<style scoped lang="scss">
+
 </style>
