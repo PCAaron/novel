6d4a9b6edd36828b28c8e96cb6d7b3182d663fba
PC_Aaron <1021048274@qq.com>
2020-08-12 21:27:37 +0800

    feat:通过provide/inject实现aside显示隐藏

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
     <div>
         <Topnav/>
         <div class="container">
-            <aside>
+            <aside v-if="asideVisible">
                 <h2>组件列表</h2>
                 <ol>
                     <li>
@@ -25,9 +25,32 @@
 </template>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
+import { inject, Ref } from 'vue'
 export default {
     components: {
         Topnav
+    },
+    setup() {
+        const asideVisible = inject<Ref<boolean>>('asideShow')
+        return {asideVisible}
     }
 }
 </script>
+<style scoped lang="scss">
+aside{
+    background: lightblue;
+    width: 150px;
+    padding: 70px 16px 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    > h2{
+        margin-bottom: 4px;
+    }
+    > ol {
+        >li {
+            padding: 4px 0
+        }
+    }
+}
+</style>
