9501bccfe383eb988ef02e1565bc5a7245b3b72c
PC_Aaron <1021048274@qq.com>
2020-08-12 21:57:08 +0800

    feat:抽取router并通过router.afterEach控制aside显示隐藏

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 
 <script lang="ts">
 import { ref, provide } from 'vue'
+import { router } from './router'
 export default {
   name: 'App',
   setup() {
     const _width = document.documentElement.clientWidth
     const asideVisible = ref( _width <= 768 ? false : true)
     provide('asideShow', asideVisible)
+    router.afterEach(() => {
+      if(_width <= 768) {
+        asideVisible.value = false
+      }
+    })
   }
 }
 </script>
