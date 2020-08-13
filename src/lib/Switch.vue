6a5093b3a532d6f5dd5715cf8bc9e78a3fdcd286
PC_Aaron <1021048274@qq.com>
2020-08-13 21:26:10 +0800

    feat:初始化switch

---
+++ src/lib/Switch.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button :class="{checked: isCheck}" @click="toggleSwitch"><span></span></button>
+    </div>
+</template>
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    setup() {
+        const isCheck = ref(false)
+        const toggleSwitch = () =>{
+            isCheck.value = !isCheck.value
+        }
+        return {
+            isCheck,
+            toggleSwitch
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button{
+    height: $h;
+    width: $h*2;
+    border:none;
+    background: grey;
+    border-radius: $h/2;
+    position: relative;
+    span{
+        position: absolute;
+        top:2px;
+        left:2px;
+        height:$h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2/2;
+        transition: left 250ms ease;
+    }
+    &.checked{
+        background: blue;
+        > span{
+            left: calc(100% - #{$h2} - 2px);
+        }
+    }
+}
+button:focus{
+    outline: none
+}
+</style>
